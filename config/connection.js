ce721be0efac0c987aa9d763c48bc1eb8b5f022f
William Whitted <youngedre@gmail.com>
2019-10-10 14:32:54 -0400

    adding config file containing mysql connection and base orm.js, no orms at the moment

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost", 
+    port: 3306,
+    user: "root", 
+    password: "12345678",
+    database: "burger_db",
+})
+
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting: "+err.stack)
+        return;
+    }
+    console.log("Connected as id "+ connection.threadId);
+});
+
+module.exports = connection;
