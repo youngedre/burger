27d2c3583b7700c040299cfd5650b8befa7a92e1
William Whitted <youngedre@gmail.com>
2019-10-10 15:31:57 -0400

    added routes for burger-controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+
+    router.get("/", function(req, res){
+        burger.selectAll(function(data){
+            var dbData = {
+                burgers: data
+            };
+            console.log(dbData);
+            res.render("index", dbData);
+        }); 
+    });
+    router.post("/api/burgers", function(req, res){
+        burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result){
+            res.json({ id: result.insertId });
+        });
+    });
+
+    router.put("/api/burgers/:id", function(req, res) {
+        var condition = "id = " + req.params.id;
+    
+        console.log("condition", condition);
+    
+        burger.update({
+            devoured: req.body.devoured
+        }, condition, function(result) {
+            if(result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+
+
+
+
+module.exports = router;
